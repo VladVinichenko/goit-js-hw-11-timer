f8d9b03abe93fff1f1c03394c53d7ad725f91805
VladVinichenko <vinnyxxxl925@gmail.com>
2021-10-15 21:25:49 +0300

    Complete v1

--- src/js/timer.js
+++ src/js/timer.js
@@ -1,77 +1,103 @@
 const refs = {
   body: document.querySelector('body'),
-  secs: document.querySelector('[data-value="secs"]'),
+  data: document.querySelector('[data-value="data"]'),
   start: document.querySelector('[data-action="start"]'),
-  stop: document.querySelector('[data-action="stop"]'),
+  timerBox: document.querySelector('.timer__box'),
+  formBox: document.querySelector('.form-box'),
 };
 
-// new CountdownTimer({
-//   selector: '#timer-1',
-//   targetDate: new Date('Jul 17, 2019'),
-// });
-/*
- * Оставшиеся дни: делим значение UTC на 1000 * 60 * 60 * 24, количество
- * миллисекунд в одном дне (миллисекунды * секунды * минуты * часы)
- */
-// const days = Math.floor(time / (1000 * 60 * 60 * 24));
-
-/*
- * Оставшиеся часы: получаем остаток от предыдущего расчета с помощью оператора
- * остатка % и делим его на количество миллисекунд в одном часе
- * (1000 * 60 * 60 = миллисекунды * минуты * секунды)
- */
-// const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-
-/*
- * Оставшиеся минуты: получаем оставшиеся минуты и делим их на количество
- * миллисекунд в одной минуте (1000 * 60 = миллисекунды * секунды)
- */
-// const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
-
-/*
- * Оставшиеся секунды: получаем оставшиеся секунды и делим их на количество
- * миллисекунд в одной секунде (1000)
- */
-// const secs = Math.floor((time % (1000 * 60)) / 1000);
-
 class CountdownTimer {
-  constructor(dd, hh, mm, ss) {
-    /*
-     * Оставшиеся дни: делим значение UTC на 1000 * 60 * 60 * 24, количество
-     * миллисекунд в одном дне (миллисекунды * секунды * минуты * часы)
-     */
-    const days = Math.floor(time / (1000 * 60 * 60 * 24));
+  constructor(setTime) {
+    const selector = setTime.selector;
+    const time = setTime.targetDate.getTime();
+    const timerRefs = {
+      days: document.querySelector(`${selector}`).querySelector('[data-value="days"]'),
+      hours: document.querySelector(`${selector}`).querySelector('[data-value="hours"]'),
+      mins: document.querySelector(`${selector}`).querySelector('[data-value="mins"]'),
+      secs: document.querySelector(`${selector}`).querySelector('[data-value="secs"]'),
+    };
+    let started = false; //started timer
+    let timerId = null;
+    let changeTime = null;
+    function start() {
+      if (!started) {
+        timerId = setInterval(newValue, 1000);
+        started = true;
+        return;
+      }
+    }
 
-    /*
-     * Оставшиеся часы: получаем остаток от предыдущего расчета с помощью оператора
-     * остатка % и делим его на количество миллисекунд в одном часе
-     * (1000 * 60 * 60 = миллисекунды * минуты * секунды)
-     */
-    const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    function newValue() {
+      if (Date.now() < time) {
+        changeTime = time - Date.now();
+        // console.log(changeTime); //==============change time
+        updateTime(changeTime);
+        return;
+      }
+      clearInterval(timerId);
+      started = false;
+      // console.log('timer-stop', `${time}`);
+      return;
+    }
+    function pad(value) {
+      return String(value).padStart(2, '0');
+    }
+    function updateTime(currentTime) {
+      //formatting UNIX time to dd.. hh mm ss
+      const days = pad(Math.floor(currentTime / (1000 * 60 * 60 * 24)));
+      const hours = pad(Math.floor((currentTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+      const mins = pad(Math.floor((currentTime % (1000 * 60 * 60)) / (1000 * 60)));
+      const secs = pad(Math.floor((currentTime % (1000 * 60)) / 1000));
+      getValue(days, hours, mins, secs);
+    }
+    function getValue(days, hours, mins, secs) {
+      timerRefs.days.innerHTML = days;
+      timerRefs.hours.innerHTML = hours;
+      timerRefs.mins.innerHTML = mins;
+      timerRefs.secs.innerHTML = secs;
+    }
 
-    /*
-     * Оставшиеся минуты: получаем оставшиеся минуты и делим их на количество
-     * миллисекунд в одной минуте (1000 * 60 = миллисекунды * секунды)
-     */
-    const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
-
-    /*
-     * Оставшиеся секунды: получаем оставшиеся секунды и делим их на количество
-     * миллисекунд в одной секунде (1000)
-     */
-    const secs = Math.floor((time % (1000 * 60)) / 1000);
+    start();
   }
 }
-const date = new Date('Jul 17, 2019');
-console.log(date);
 
-const timerId = setInterval(timeactive, 1000);
+function timer() {
+  let started = false; //is started
 
-function timeactive() {
-  refs.secs.parentNode.classList.add('field_click');
-  const timerTick = setTimeout(timeOutTick, 150);
-  function timeOutTick() {
-    clearTimeout(timerTick);
-    refs.secs.parentNode.classList.remove('field_click');
+  refs.data.addEventListener('change', changeData);
+
+  function changeData() {
+    //action to change data in form
+    if (Date.now() < refs.data.valueAsNumber) {
+      //comparing two dates(now and input)
+      refs.start.removeAttribute('disabled');
+      refs.start.addEventListener('click', start);
+      return;
+    }
+    refs.start.setAttribute('disabled', true);
+    refs.start.removeEventListener('click', start);
   }
+
+  function start() {
+    if (!started) {
+      started = true;
+      refs.timerBox.classList.remove('is-hide');
+      refs.formBox.classList.add('is-hide');
+      refs.start.setAttribute('disabled', true);
+      //=========of class
+      new CountdownTimer({
+        selector: '#timer-1',
+        targetDate: new Date(refs.data.valueAsNumber),
+      });
+      //=========of class
+    }
+  }
+  function initPage() {
+    //start settings on download page
+    refs.start.setAttribute('disabled', true);
+  }
+
+  initPage(); //set start settings on download page
 }
+
+timer(); //start script.
